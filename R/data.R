fb1abfc7a35642c20303b4f3c4eadf4b5477e53e
Nussaibah Raja-Schoob <nussaibah.raja.schoob@fau.de>
2019-11-04 11:58:49 +0100

    "dems" added to data and "clim" removed

--- R/data.R
+++ R/data.R
@@ -1,16 +1,10 @@
-#' Temperature dataset from modelled simulations 
+#' PaleoDEM rasters from the reconstructions Chris Scotese.
 #'
-#' A dataset containing temperature data from Paul Valdes and Chris Scotese for the time
-#' 0, 4 and 11 Ma. 
+#' A dataset containing the paleoDEM reconstructions of Chris Scotese for the time interval 0 - 45 Ma. 
 #'
-#' @format A raster brick of 3 layers corresponding to time intervals.
-#' \describe{
-#'   \item{X0}{temperature data for 0 Ma}
-#'   \item{X4}{temperature data for 4 Ma}
-#'   \item{X11}{temperature data for 11 Ma}
-#' }
-#' @source Valdes simulations
-"clim"
+#' @format A RasterArray with 10 layers.
+#' @source Chris Scotese
+"dems"
 
 
 #' Colour gradient ramps for default plotting 
