dfca15c001b7c2cb55132aa1eaaff71bb977ad0b
adamkocsis <adam.kocsis@fau.de>
2019-11-28 19:20:58 +0100

    added dummy 'clim' for vignette and example dev

--- R/data.R
+++ R/data.R
@@ -5,3 +5,11 @@
 #' @format A RasterArray with 10 layers.
 #' @source Chris Scotese
 "dems"
+
+#' Annual mean temperature (bio1) and precipitation (bio12) variables between 2001 and 2010 from the CHELSA model
+#'
+#' Currently this is just a dummy made from the DEMS.
+#' @format A RasterArray with 20 layers, 10 rows and two columns.
+#' @source DEMS
+"clim"
+
