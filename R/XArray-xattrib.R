c1e74b8ab03b8a477076b09a7920ccb652baada5
adamkocsis <adam.t.kocsis@gmail.com>
2020-09-25 16:25:04 +0200

    GEneralized colnames(), rownames() and colnames<- and rownames<-

--- R/XArray-xattrib.R
+++ R/XArray-xattrib.R
@@ -94,3 +94,107 @@ setMethod(
 	function(x) nlayers(x@stack)
 )
 
+
+#####################
+
+#' Column names of two-dimensional \code{\link{RasterArray}} or \code{\link{SpatialArray}}
+#' 
+#' Get or set the column names of two-dimensional code{\link{RasterArray}} or \code{\link{SpatialArray}} objects 
+#' @param x \code{\link{RasterArray}} or \code{\link{SpatialArray}} object.
+#' @param value \code{character} vector.
+#' @return A \code{character} vector of column names or \code{NULL}.
+#' 
+#' @examples
+#' data(clim)
+#' colnames(clim)
+#' colnames(clim) <- c("a", "b")
+#' @rdname colnames
+#' @exportMethod colnames
+setMethod(
+	"colnames",
+	signature="XArray",
+	function(x) colnames(x@index)
+)
+
+#' @rdname colnames
+#' @exportMethod "colnames<-"
+setReplaceMethod(
+	"colnames",
+	signature="XArray",
+	definition=function(x,  value){
+		# not defined for matrices or higher
+		if(length(dim(x))!=2) stop("The proxy is not a 2D matrix.")
+		colnames(x@index) <- value 
+		return(x)
+})
+
+
+
+#' Row names of two-dimensional \code{\link{RasterArray}} or \code{\link{SpatialArray}} objects
+#' 
+#' Get or set the row names of two-dimensional \code{\link{RasterArray}} or \code{\link{SpatialArray}}objects  
+#' @param x \code{\link{RasterArray}} or \code{\link{SpatialArray}} object.
+#' @param value \code{character} vector.
+#' @return A \code{character} vector of row names or \code{NULL}.
+#' 
+#' @examples
+#' data(clim)
+#' rownames(clim)
+#' rownames(clim) <- paste("year", rownames(clim))
+#' @rdname rownames
+#' @exportMethod rownames
+setMethod(
+	"rownames",
+	signature="XArray",
+	function(x) rownames(x@index)
+)
+
+#' @rdname rownames
+#' @exportMethod "rownames<-"
+setReplaceMethod(
+	"rownames",
+	signature="XArray",
+	definition=function(x,  value){
+		# not defined for matrices or higher
+		if(length(dim(x))!=2) stop("The proxy is not a 2D matrix.")
+		rownames(x@index) <- value 
+		return(x)
+})
+
+
+#' Names of multidimensional \code{\link{RasterArray}} or \code{\link{SpatialArray}}objects.
+#' 
+#' Get or set the dimnames of multidimensional \code{\link{RasterArray}} or \code{\link{SpatialArray}} objects 
+#' @param x \code{\link{RasterArray}} or \code{\link{SpatialArray}} object.
+#' @param value \code{character} vector.
+#' @return A \code{list} of \code{character} vectors or \code{NULL}.
+#' 
+#' @examples
+#' data(dems)
+#' dimnames(dems)
+#' data(clim)
+#' dimnames(clim)
+#' dimnames(clim)[[2]] <- c("first", "second")
+#' @rdname dimnames
+#' @exportMethod dimnames
+setMethod(
+	"dimnames",
+	signature="XArray",
+	function(x) dimnames(x@index)
+)
+
+#' @rdname dimnames
+#' @exportMethod "dimnames<-"
+setReplaceMethod(
+	"dimnames",
+	signature="XArray",
+	definition=function(x,  value){
+		# not defined for matrices or higher
+		if(is.null(dim(x))) stop("One-dimensional RasterArrays and SpatialArrays have no dimnames.")
+		dimnames(x@index) <- value 
+		return(x)
+})
+
+
+
+
