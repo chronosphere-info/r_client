c16785885f0a4f020b309708961133f12557ea53
adamkocsis <adam.t.kocsis@gmail.com>
2023-06-27 17:44:48 +0200

    before checkout

--- pkgdown/code/headChange.py
+++ pkgdown/code/headChange.py
@@ -8,13 +8,13 @@ with open(filename, 'r') as file :
 # Replace the target string
 filedata = filedata.replace('<a class="navbar-brand me-2" href="index.html">chronosphere</a>',
                             '''<a class="navbar-brand me-2" href="https://chronosphere.info/" style="padding-right:8px">
-		  <img style="margin-right:8px" src="images/chronosphere_luminosity_500_hover.png" width="30px" height="30px">chronosphere</a> ›  
-		<a style="margin-left:16px" class="navbar-brand" href="index.html"> <img style="margin-right:6px;position: relative; top: -2px;" src="images/r_luminosity_hover.png" width="25px" height="25px">client </a>''')
+		  <img style="margin-right:8px;margin-top:-2px" src="images/chronosphere_luminosity_500_hover.png" width="30px" height="30px">chronosphere</a> ›  
+		<a style="margin-left:16px" class="navbar-brand" href="index.html"> <img style="margin-right:6px;position: relative; top: -4px;" src="images/r_luminosity_hover.png" width="25px" height="25px">client </a>''')
 
 filedata = filedata.replace('<a class="navbar-brand me-2" href="../index.html">chronosphere</a>',
                             '''<a class="navbar-brand me-2" href="https://chronosphere.info/" style="padding-right:8px">
-		  <img style="margin-right:8px" src="../images/chronosphere_luminosity_500_hover.png" width="30px" height="30px">chronosphere</a> ›  
-		<a style="margin-left:16px" class="navbar-brand" href="../index.html"> <img style="margin-right:6px;position: relative; top: -2px;" src="../images/r_luminosity_hover.png" width="25px" height="25px">client </a>''')
+		  <img style="margin-right:8px;margin-top:-2px" src="../images/chronosphere_luminosity_500_hover.png" width="30px" height="30px">chronosphere</a> ›  
+		<a style="margin-left:16px" class="navbar-brand" href="../index.html"> <img style="margin-right:6px;position: relative; top: -4px;" src="../images/r_luminosity_hover.png" width="25px" height="25px">client </a>''')
 
 # Write the file out again
 with open(filename, 'w') as file:
